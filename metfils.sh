a4e18ac967f10bcab728639869ccc4872564b51c
Julio Pastor Marcelo <final.africae@gmail.com>
2016-01-07 22:51:49 -0600

    subiendo cambios y un script

---
+++ metfils.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create directory if it doesn't exist
+if [ ! -d "metafiles" ]; then
+  mkdir metafiles
+fi
+
+# Run pdflatex and biber with metafiles as in-/output directories
+pdflatex -output-directory=metafiles main
+biber --input-directory=metafiles --output-directory=metafiles main
+pdflatex -output-directory=metafiles main
+pdflatex -output-directory=metafiles main
+makeglossaries -d metafiles main
+pdflatex -output-directory=metafiles main
+
+# Create a softlink to the output PDF
+ln -s metafiles/main.pdf
