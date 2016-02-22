064da7e8ffab8bffdd06f4b7fa104e69d149c6c5
Julio Pastor Marcelo <final.africae@gmail.com>
2016-02-22 13:19:38 -0600

    modificacion de git ignorer y subiendo un script para procesar los textos y mandar la salida a una carpta llamada metafile

--- metfils.sh
+++ metfils.sh
@@ -8,8 +8,6 @@ fi
 # Run pdflatex and biber with metafiles as in-/output directories
 pdflatex -output-directory=metafiles main
 biber --input-directory=metafiles --output-directory=metafiles main
-pdflatex -output-directory=metafiles main
-pdflatex -output-directory=metafiles main
 makeglossaries -d metafiles main
 pdflatex -output-directory=metafiles main
 
